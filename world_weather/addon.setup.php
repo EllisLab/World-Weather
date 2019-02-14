cbeddbc24914a3dd00b72c7bb4108be9ded0634d
robinsowell <robin.sowell@ellislab.com>
2019-02-14 18:38:26 -0500

    Making compliant with their new SSL protocol- version bump

--- world_weather/addon.setup.php
+++ world_weather/addon.setup.php
@@ -5,7 +5,7 @@ return array(
 	'author_url'  => 'https://ellislab.com/',
 	'name'        => 'World Weather',
 	'description' => 'Displays weather conditions on your site',
-	'version'     => '3.0.1',
+	'version'     => '3.0.2',
 	'namespace'   => 'User\Addons\WorldWeather',
 	'settings_exist' => FALSE
 );
